2341d3439389aed36f8f7f06e524a60344ced1ad
Vincent Son <107471539+szw0407@users.noreply.github.com>
2023-11-07 21:16:07 +0800

    202112-03 90 TLE

--- CCF_CSP/202112-3.py
+++ CCF_CSP/202112-3.py
@@ -38,6 +38,31 @@ if NUM_OF_900 == w:
     NUM_OF_900 = 0
 res.extend([900] * NUM_OF_900) # add 900s
 res[0] = len(res)
-
 for i in res:
     print(i)
+if s >=0:
+    g=[1]
+    s=2**(s+1)
+    res.extend([0] * s)
+    for i in range(1, s+1):
+        g.append(0)
+        p = -3**(i)
+        k = [i for i in g]
+        for j in range(1, i+1):
+            k[j] += p*g[j-1]
+        g=k
+    for i in range(res[0]):
+        q = res[i]
+        for j in range(len(g)):
+            res[i+j] -= q*g[j]
+            res[i+j] = res[i+j] % 929
+        # ax**5+bx**4+cx**3+dx**2+e
+        # x**2-12x+27
+        # ax**3 
+        # ax**5 - 12a x**4 + 27ax**3
+        # (b+12a) x**4 + (c-27a) x**3 +dx**2+e
+        # (b+12a) x**2 
+        # (b+12a) x**4 - 12 * (b+12a) x**3 + 27 * (b+12a) x**2
+        # 
+    for i in range(-s, 0, 1):
+        print(res[i])

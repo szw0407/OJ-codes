e9816d579103a7069eec6313650e0fffc12358b7
szw0407 <107471539+szw0407@users.noreply.github.com>
2026-03-01 02:13:29 +0800

    LC100 234 AC

---
+++ LeetCN/234.palindrome-linked-list.cpp
@@ -0,0 +1,86 @@
+// @lcpr-before-debug-begin
+
+
+
+
+// @lcpr-before-debug-end
+
+/*
+ * @lc app=leetcode.cn id=234 lang=cpp
+ * @lcpr version=30204
+ *
+ * [234] 回文链表
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// struct ListNode {
+//     int val;
+//     ListNode *next;
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> ls;
+        while(head){
+            ls.push_back(head->val);
+            head = head->next;
+        }
+        auto k = ls.size() -1;
+        auto j = 0;
+        bool res = true;
+        while (k > j && res) {
+            res = res && (ls[k] == ls[j]); 
+            k--;
+            j++;
+        }
+        return res;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,2,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n
+// @lcpr case=end
+
+ */
+

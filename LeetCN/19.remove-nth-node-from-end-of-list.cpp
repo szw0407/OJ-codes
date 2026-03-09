98f2437becd4ce39204fdd661495b1a977410a6e
szw0407 <107471539+szw0407@users.noreply.github.com>
2026-03-09 20:07:51 +0800

    LC 19 AC

---
+++ LeetCN/19.remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,94 @@
+// @lcpr-before-debug-begin
+
+
+
+
+// @lcpr-before-debug-end
+
+/*
+ * @lc app=leetcode.cn id=19 lang=cpp
+ * @lcpr version=30204
+ *
+ * [19] 删除链表的倒数第 N 个结点
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// struct ListNode {
+//     int val;
+//     ListNode *next;
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        queue<ListNode *> q;
+        int i = 0;
+        auto pt = head;
+        if (head == nullptr) return nullptr;
+        if (head->next == nullptr){
+            if (n == 0) return head;
+            else return nullptr;
+            }
+        
+        while (pt->next != nullptr) {
+            q.push(pt);
+            pt = pt->next;
+            if (q.size() > n) q.pop();
+        }
+        if (q.size() < n) {
+            return head->next;
+        }
+        q.front() ->next = q.front()->next->next;
+        return head;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5]\n2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n1\n
+// @lcpr case=end
+
+ */
+

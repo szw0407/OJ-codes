6a079418c36bc97536e5188c750d06c1e7326abe
szw0407 <107471539+szw0407@users.noreply.github.com>
2026-03-14 01:57:09 +0800

    LC 206 AC

---
+++ LeetCN/206.reverse-linked-list.cpp
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=cpp
+ * @lcpr version=30204
+ *
+ * [206] 反转链表
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// struct ListNode {
+//     int val;
+//     ListNode *next;
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        stack<ListNode* > st;
+        auto p = head;
+        while (p)
+        {
+            st.push(p);
+            p =p->next;
+        }
+        ListNode* h = new ListNode(INT32_MIN);
+        p = h;
+        while (st.size()) {
+            p->next = st.top();
+            st.pop();
+            p = p->next;
+        }
+        p->next = nullptr;
+        return h->next;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+ */
+

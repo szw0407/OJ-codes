703ac0591395b0fd0656b32c76361e97942a0232
szw0407 <107471539+szw0407@users.noreply.github.com>
2026-03-16 13:04:48 +0800

    LC 21 AC

---
+++ LeetCN/21.merge-two-sorted-lists.cpp
@@ -0,0 +1,97 @@
+// @lcpr-before-debug-begin
+
+
+
+
+// @lcpr-before-debug-end
+
+/*
+ * @lc app=leetcode.cn id=21 lang=cpp
+ * @lcpr version=30204
+ *
+ * [21] 合并两个有序链表
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+// struct ListNode {
+//     int val;
+//     ListNode *next;
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        auto p = new ListNode(NULL);
+        auto k = p;
+        while (list1 || list2) {
+            if (list1 && list2) {
+                if (list1 -> val > list2->val) {
+                    p->next = list2;
+                    list2 = list2->next;
+                } else {
+                    p->next = list1;
+                    list1 = list1->next;
+                }
+            } else if (list1)
+            {
+                p->next = list1;
+                list1 = list1->next;
+            } else{ p->next = list2;list2 = list2->next;}
+
+            p = p->next;
+            
+        }
+        return k->next;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,4]\n[1,3,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n[]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n[0]\n
+// @lcpr case=end
+
+ */
+

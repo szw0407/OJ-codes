6580ba09c79c575626c94c64c2ab2544a6cacf44
szw0407 <107471539+szw0407@users.noreply.github.com>
2026-03-22 13:37:06 +0800

    LC 877 AC

---
+++ LeetCN/877.stone-game.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=877 lang=cpp
+ * @lcpr version=30204
+ *
+ * [877] 石子游戏
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [5,3,4,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,7,2,3]\n
+// @lcpr case=end
+
+ */
+

ac22a8015f4fe29bee08acf922ef1de34979c857
szw0407 <107471539+szw0407@users.noreply.github.com>
2026-03-23 15:04:27 +0800

    LC 26 ac

---
+++ LeetCN/26.remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=26 lang=cpp
+ * @lcpr version=30204
+ *
+ * [26] 删除有序数组中的重复项
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int l,r;
+        l = 1;
+        int cach;
+        cach = nums[0];
+        int t = 1;
+        for (r=1;r<nums.size();r++) {
+            if (nums[r] == cach) continue;
+            cach = nums[r];
+            swap(nums[r],nums[l]);
+            l++;
+            t++;
+
+        }
+        return t;
+
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,1,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0,0,1,1,1,2,2,3,3,4]\n
+// @lcpr case=end
+
+ */
+

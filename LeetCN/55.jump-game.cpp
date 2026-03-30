774b375715166edf3ec36dacec222d8e7b95d6fd
szw0407 <107471539+szw0407@users.noreply.github.com>
2026-03-30 13:24:49 +0800

    LC 55 AC

---
+++ LeetCN/55.jump-game.cpp
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=55 lang=cpp
+ * @lcpr version=30204
+ *
+ * [55] 跳跃游戏
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+
+    int term = nums.size();
+    int f = 0;
+    int i;
+    for (i=0;i<term;i++) {
+        if (f >= term-1) return true;
+        if (f == i && nums[i] == 0) return false;
+        f = max(i+nums[i], f);
+        
+    }
+    return (f>=term-1);
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [2,3,1,1,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,2,1,0,4]\n
+// @lcpr case=end
+
+ */
+

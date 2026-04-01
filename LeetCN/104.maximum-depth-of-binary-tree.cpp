9e9168e14f15f4903defb00773e56531ae81b153
szw0407 <107471539+szw0407@users.noreply.github.com>
2026-04-01 22:31:18 +0800

    LC 104 AC

---
+++ LeetCN/104.maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=cpp
+ * @lcpr version=30204
+ *
+ * [104] 二叉树的最大深度
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int max_d = 0;
+    void check_depth(TreeNode* n, int current_dpt) {
+        if (!n){ max_d = max(max_d, current_dpt);return;}
+        check_depth(n->left, current_dpt+1);
+        check_depth(n->right, current_dpt+1);
+    }
+    int maxDepth(TreeNode* root) {
+        check_depth(root,0);
+        return max_d;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [3,9,20,null,null,15,7]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,null,2]\n
+// @lcpr case=end
+
+ */
+

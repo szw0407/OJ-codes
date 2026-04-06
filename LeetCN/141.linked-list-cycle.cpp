fe767e98171b391a9ec9272a8cbf403f62390fb1
szw0407 <107471539+szw0407@users.noreply.github.com>
2026-04-06 20:25:53 +0800

    LC 141 AC

---
+++ LeetCN/141.linked-list-cycle.cpp
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=cpp
+ * @lcpr version=30204
+ *
+ * [141] 环形链表
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        int cnt = 0;
+        while (head && cnt < 10005) {
+            head = head->next;
+            cnt++;
+        }
+        if (cnt >= 10005) return true;
+        return false;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [3,2,0,-4]\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n0\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n-1\n
+// @lcpr case=end
+
+ */
+

60d2f189e81332dbfda893074aa2153a4581afbb
szw0407 <107471539+szw0407@users.noreply.github.com>
2026-04-06 22:21:07 +0800

    LC 142 AC

---
+++ LeetCN/142.linked-list-cycle-ii.cpp
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=cpp
+ * @lcpr version=30204
+ *
+ * [142] 环形链表 II
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        unordered_set<ListNode*> rec;
+        int res = -1;
+
+        while (head) {
+            if (rec.contains(head)) {
+                return head;
+            }
+            rec.insert(head);
+            head = head->next;
+        }
+        return nullptr;
+
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [3,2,0,-4]\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n0\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n-1\n
+// @lcpr case=end
+
+ */
+

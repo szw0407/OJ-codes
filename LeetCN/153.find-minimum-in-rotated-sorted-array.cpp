9f3189327096188ca7b40734207fd4f2032a3b35
szw0407 <107471539+szw0407@users.noreply.github.com>
2026-04-08 20:43:39 +0800

    LC 153 AC

--- LeetCN/153.find-minimum-in-rotated-sorted-array.cpp
+++ LeetCN/153.find-minimum-in-rotated-sorted-array.cpp
@@ -1,3 +1,10 @@
+// @lcpr-before-debug-begin
+
+
+
+
+// @lcpr-before-debug-end
+
 /*
  * @lc app=leetcode.cn id=153 lang=cpp
  * @lcpr version=30204
@@ -23,21 +30,48 @@ using namespace std;
 #include <unordered_set>
 #include <utility>
 #include <vector>
+#include <cmath>
 
 // @lcpr-template-end
 // @lc code=start
-class cmp {
-    public:
-    bool operator()(const int& a, const int& b) const {
-        return a>b;
-    }
-};
+// class cmp {
+//     public:
+//     bool operator()(const int& a, const int& b) const {
+//         return a>b;
+//     }
+// };
+// #define i (ceil((l+r)/2.0))
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        priority_queue<int, vector<int>, cmp> data;
-        for (auto n:nums) data.push(n);
-        return data.top();
+        // priority_queue<int, vector<int>, cmp> data;
+        // for (auto n:nums) data.push(n);
+        // return data.top();
+        const auto len = nums.size();
+        auto q = nums.back();
+        int l = 0;
+        int r = len - 1;
+        int i;
+        while (l < r) {
+            bool t = true;
+            i = (((l+r)/2));
+            if (i > 0) {
+                if (nums[i-1] < nums[i]) t = false;
+            } 
+            if (i < len-1) {
+                if (nums[i] > nums[i+1]) t = false;
+            }
+            if (t) return nums[i];
+            
+            if (nums[i] > q) {
+                l = i+1;
+            } else {
+                
+                r = i - 1;
+            }
+        }
+        return nums[(((l+r)/2))];
+
     }
 };
 // @lc code=end
@@ -57,5 +91,8 @@ public:
 // [11,13,15,17]\n
 // @lcpr case=end
 
+// @lcpr case=start
+// [2,1]\n
+// @lcpr case=end
  */
 

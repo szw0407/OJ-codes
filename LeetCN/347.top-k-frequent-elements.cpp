fc94594371fd6d8bf5c2b722716a0d18deff21dc
szw0407 <107471539+szw0407@users.noreply.github.com>
2026-04-10 21:30:46 +0800

    LC 347 AC

--- LeetCN/347.top-k-frequent-elements.cpp
+++ LeetCN/347.top-k-frequent-elements.cpp
@@ -25,32 +25,29 @@ using namespace std;
 #include <vector>
 // @lcpr-template-end
 // @lc code=start
-class cmp {
-    public:
-        bool operator() (const pair<int, int> &a, const pair<int,int> &b) const {
-            return a.second < b.second;
-        }
-};
+bool cmp(const pair<int, int>& a, const pair<int, int>& b) {return a.second > b.second;};
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, pair<int, int> > cnt;
-        for (auto n : nums) {
-            if (!cnt.contains(n)) {
-                cnt[n] = pair<int, int>(n, 1);
-            } else cnt[n].second++;
-        }
-        priority_queue<pair<int, int>, vector<pair<int, int> >, cmp> pq;
-        for (auto p : cnt) {
-            pq.push(p.second);
-        }
-        vector<int> res;
-        for (auto i = 0;i<k;i++) {
-            res.push_back(pq.top().first);
-            pq.pop();
-        }
-        return res;
 
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> counts;
+        for (int n : nums) {
+            counts[n]++;
+        }
+
+        vector<pair<int, int>> v;
+        for (auto& it : counts) {
+            v.push_back(it);
+        }
+        nth_element(v.begin(), v.begin() + k - 1, v.end(), cmp);
+
+        vector<int> res;
+        for (int i = 0; i < k; ++i) {
+            res.push_back(v[i].first);
+        }
+
+        return res;
     }
 };
 // @lc code=end
